3c6fa7ffef51db1c30924e77537a16ef3672236b
dangnm-2032 <dangnm.working@gmail.com>
2024-10-12 12:14:43 +0700

    init branch

---
+++ transmiter_inference.py
@@ -0,0 +1,137 @@
+import numpy as np  # Module that simplifies computations on matrices
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Activation, Permute, Dropout, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import SeparableConv2D, DepthwiseConv2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import SpatialDropout2D
+from tensorflow.keras.regularizers import l1_l2
+from tensorflow.keras.layers import Input, Flatten
+from tensorflow.keras.constraints import max_norm
+from tensorflow.keras import backend as K
+
+from models.EEGNet import *
+
+from preprocessing import *
+from utils import *
+
+import joblib
+
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='COM51',
+    #'/dev/tty.usbserial-10',
+    baudrate=9600,
+    timeout=.1
+)
+ser.isOpen()
+
+##################### SCALER #########################################
+import joblib
+label_name = ['eyebrows', 'left', 'right', 'both', 'teeth']
+scalers = {}
+for label in label_name:
+    scalers[label] = joblib.load(rf'.\pipeline_{label}\checkpoints\scaler.save')
+######################################################################
+
+n_timesteps = 128
+
+####################### MODEL ############################################
+model = load_model(r'.\checkpoints\orthogonal_false.keras')
+####################### END MODEL ########################################
+
+####### FILTER #############################################
+filters = {
+    'left': filter_left,
+    'right': filter_right,
+    'both': filter_both,
+    'teeth': filter_teeth,
+    'eyebrows': filter_eyebrows,
+}
+############################################################
+
+""" 1. CONNECT TO EEG STREAM """
+# Search for active LSL streams
+print('Looking for an EEG stream...')
+streams = resolve_byprop('type', 'EEG', timeout=2)
+if len(streams) == 0:
+    raise RuntimeError('Can\'t find EEG stream.')
+print("Start acquiring data")
+inlet = StreamInlet(streams[0], max_chunklen=128)
+eeg_time_correction = inlet.time_correction()
+
+n_timesteps = 128
+
+while True:
+    # inlet.pull_chunk(timeout=1.0, max_samples=128)
+    eeg_data, timestamp = inlet.pull_chunk(timeout=1.0, max_samples=n_timesteps)
+    eeg_data = np.array(eeg_data)[:, :-1]  # sample, channel
+
+    ########################### Preprocessing ###################################
+    x_eyebrows = pipeline(eeg_data, filters['eyebrows'], scalers['eyebrows'])
+    x_left = pipeline(eeg_data, filters['left'], scalers['left'])
+    x_right = pipeline(eeg_data, filters['right'], scalers['right'])
+    x_both = pipeline(eeg_data, filters['both'], scalers['both'])
+    x_teeth = pipeline(eeg_data, filters['teeth'], scalers['teeth'])
+    x = np.concatenate(
+        [
+            x_eyebrows,
+            x_left,
+            x_right,
+            x_both,
+            x_teeth
+        ],
+        axis=1
+    )
+    
+    input = np.concatenate([x], axis=1)
+    input = np.expand_dims(input, 0)
+    input = np.expand_dims(input, -1)
+    input = input.transpose(0, 2, 1, 3)
+    # print(input.shape)
+    assert input.shape == (1, 20, 128, 1)
+    #############################################################################
+
+
+    ############################### Inference ###################################
+    y_pred = model.predict([
+        input[:, :4], 
+        input[:, 4:8], 
+        input[:, 8:12],
+        input[:, 12:16],
+        input[:, 16:20]
+    ])
+    y_pred = np.argmax(y_pred, 2)[0]
+    #############################################################################
+    print(y_pred)
+    count = [0, 0, 0, 0, 0, 0]
+
+    temp = np.unique(y_pred, return_counts=True)
+    for i in range(temp[0].shape[0]):
+        count[temp[0][i]] = temp[1][i]
+    count[0] = 0
+    pred = np.argmax(count)
+
+    if pred == 1:
+        print('Eyebrows - Go backward')
+        ser.write(bytes('2', 'ascii'))
+    elif pred == 2:
+        print('Left - Turn left')
+        ser.write(bytes('5', 'ascii'))
+    elif pred == 3:
+        print('Right - Turn right')
+        ser.write(bytes('6', 'ascii'))
+    elif pred == 4:
+        print('Both - Stop')
+        ser.write(bytes('0', 'ascii'))
+    elif pred == 5:
+        print('Teeth - Go forward')
+        ser.write(bytes('1', 'ascii'))
---
+++ utils.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+def process_raw_record(args):
+    input_path = args[0]
+    label_path = args[1]
+
+    input_df = pd.read_csv(input_path).drop(columns=['timestamps', 'Right AUX'])
+    input_data = input_df.to_numpy()
+
+    label_df = pd.read_csv(label_path).to_numpy()
+    label = np.zeros(len(input_data))
+    
+    for row in label_df:
+        label[row[0]:row[1]] = row[2]
+
+    return input_data, label
+
+
+def process_raw_record_20_features(raw_data, drop_cols=[]):
+    input_data = []
+    label = []
+
+    for data in raw_data:
+
+        input_path = raw_data[data][0]
+        label_path = raw_data[data][1]
+
+        input_df = pd.read_csv(input_path).drop(columns=['timestamps', 'Right AUX', ]  + drop_cols)
+        _input_data = input_df.to_numpy()
+
+        label_df = pd.read_csv(label_path).to_numpy()
+        _label = np.zeros(len(_input_data))
+        
+        for row in label_df:
+            _label[row[0]:row[1]] = row[2]
+
+        input_data.append(_input_data)
+        label.append(_label)
+    
+    input_data = np.concatenate(input_data)
+    label = np.concatenate(label)
+    
+    return input_data, label
+
+
+def pipeline(x, filter, scaler, i=None, time_step=None):
+    if i is None:
+        i = 0
+    if time_step is None:
+        time_step = x.shape[0]
+    x_new = x[i:i+time_step].copy()
+    for col in range(x_new.shape[1]):
+        x_new[:, col] = filter(x_new[:, col])
+    x_new = scaler.transform(x_new)
+
+    return x_new
+
+
+def create_dataset(x, y, filter, scaler, time_step=128, epsilon=0):
+    assert x.shape[0] == y.shape[0]
+    x_new = []
+    y_new = []
+
+    for i in range(0, x.shape[0] - time_step):
+        if 1 in y[i:i+time_step] or np.random.random() < epsilon:
+            x_new.append(pipeline(x, filter, scaler, i, time_step))
+            y_new.append(y[i:i+time_step])
+    
+    return np.array(x_new), np.array(y_new)
+
+def create_dataset_20_features(x, y, filter, scalers, time_step=128, epsilon=0):
+    assert x.shape[0] == y.shape[0]
+    x_new = []
+    y_new = []
+
+    for i in range(0, x.shape[0] - time_step):
+        if 1 in y[i:i+time_step] or np.random.random() < epsilon:
+            x_eyebrows = pipeline(x, filter['eyebrows'], scalers['eyebrows'], i, time_step)
+            x_left = pipeline(x, filter['left'], scalers['left'], i, time_step)
+            x_right = pipeline(x, filter['right'], scalers['right'], i, time_step)
+            x_both = pipeline(x, filter['both'], scalers['both'], i, time_step)
+            x_teeth = pipeline(x, filter['teeth'], scalers['teeth'], i, time_step)
+            x_new.append(
+                np.concatenate(
+                    [
+                        x_eyebrows,
+                        x_left,
+                        x_right,
+                        x_both,
+                        x_teeth
+                    ],
+                    axis=1
+                )
+            )
+            y_new.append(y[i:i+time_step])
+    
+    return np.array(x_new), np.array(y_new)
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+def get_input(df, filter, scaler):
+    # Split then filtered
+    n_timesteps = 128
+    data = df.to_numpy()
+    input_data = []
+    for i in range(0, data.shape[0] // n_timesteps * n_timesteps, n_timesteps):
+        
+        x = pipeline(data, filter, scaler, i, n_timesteps)
+
+        input = np.concatenate([
+            x
+        ], axis=1)
+        input_data.append(input)    
+
+    input_data = np.array(input_data)
+    input_data = input_data[:, :, :, np.newaxis]
+    input_data = input_data.transpose(0, 2, 1, 3)
+    print(input_data.shape)
+
+    data = input_data[:, :, :, 0].transpose(0, 2, 1)
+    data = np.concatenate(data, axis=0)
+    print(data.shape)
+    return data, input_data
+
+def get_output(input_data, model):
+    y_pred = model.predict(input_data)
+    y_pred = np.concatenate(y_pred, axis=0)
+    y_pred_onehot = y_pred
+
+    return y_pred_onehot
+
+def plot_data_result(data, y_pred_onehot, title, path):
+    plt.figure(figsize=(50, 30)).suptitle(title, fontsize=40)
+    plt.subplot(6, 1, 1)
+    plt.title("TP9", fontsize=40)
+    plt.plot(data[:, 0])
+    plt.subplot(6, 1, 2)
+    plt.title("AF7", fontsize=40)
+    plt.plot(data[:, 1])
+    plt.subplot(6, 1, 3)
+    plt.title("AF8", fontsize=40)
+    plt.plot(data[:, 2])
+    plt.subplot(6, 1, 4)
+    plt.title("TP10", fontsize=40)
+    plt.plot(data[:, 3])
+    plt.subplot(6, 1, 5)
+    plt.title("Result", fontsize=40)
+    plt.ylim(0, 1)
+    plt.plot(y_pred_onehot[:, 1])
+    plt.savefig(path)
