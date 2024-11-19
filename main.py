8183bc5eab2efa1bf8fa950fcb5fde2fbece3b64
dangnm-2032 <dangnm.working@gmail.com>
2024-11-19 15:29:24 +0700

    feat: main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from FPTU_FA24_EEG_Artifacts_Recognition.config import *
 from FPTU_FA24_EEG_Artifacts_Recognition.pipeline import *
 
-
 config_manager = ConfigurationManager()
 
 pipeline = DataPreparationPipeline(config_manager)
+pipeline.main()
+
+pipeline = TrainingPipeline(config_manager)
+pipeline.main()
+
+pipeline = EvaluationPipeline(config_manager)
 pipeline.main()
